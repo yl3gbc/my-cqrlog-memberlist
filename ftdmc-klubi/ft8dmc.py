cf2246a8fff3143add33e9de02a39e2bead11232
Arnis Armans <yl3gbc@yl3gbc.lv>
2025-10-22 20:15:00 +0300

    ftdmc-klubi

---
+++ ftdmc-klubi/ft8dmc.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests
+
+# Fetch the data from the URL
+url = "https://epc-mc.de/FT8DMC.php"
+response = requests.get(url)
+data = response.content.decode('utf-8-sig')
+
+# Split the data by lines and then by semicolons
+lines = data.strip().split("\n")
+data_list = [line.split(";") for line in lines]
+
+# Create a DataFrame from the list
+df = pd.DataFrame(data_list, columns=['num', 'id'])
+df = df[["id", "num"]]
+print(df)
+# Save DataFrame to CSV
+df.to_csv('ft8dmc.txt', index=False, sep=';', encoding='utf-8')
